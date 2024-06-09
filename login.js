aada5549e84951b5a4f3187ec630df90fcd71cd7
vtzad <vtzada12@hotmail.com>
2024-06-09 16:50:51 -0300

    tudo atualizado

---
+++ login.js
@@ -0,0 +1,25 @@
+document.getElementById('loginForm').onsubmit = function(event) {
+    event.preventDefault();
+
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+    var storedLogin = localStorage.getItem('login');
+    var storedSenha = localStorage.getItem('senha');
+    var message = document.getElementById('message');
+
+    message.style.display = 'none';
+
+    if (login === storedLogin && senha === storedSenha) {
+        message.innerText = "Login bem-sucedido!";
+        message.className = "success-message";
+        message.style.display = 'block';
+        // Redirecionar para a página principal
+        setTimeout(() => {
+            window.location.href = 'index.html';
+        }, 2000);
+    } else {
+        message.innerText = "Login ou senha incorretos.";
+        message.className = "error-message";
+        message.style.display = 'block';
+    }
+};
